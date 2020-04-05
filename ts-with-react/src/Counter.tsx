5d8d77139f05fbde28071fb349b60b2f354a7ddb
jake <guswns1853@gmail.com>
2020-04-05 16:54:27 +0900

    generics를 사용해야하는 경우

--- ts-with-react/src/Counter.tsx
+++ ts-with-react/src/Counter.tsx
@@ -2,6 +2,14 @@ import React, {useState} from "react";
 
 const Counter = () => {
   const [count, setCount] = useState<number>(0);
+  // useState 사용시 generics 를 사용하지 않아도 타입 추론을 잘하기 때문에 생략해도 무관
+  // 사용해야 될 상황
+  // 1. 상태가 null 일 수도, 아닐수도 있는 경우
+  // type Information = {name: string; description: string};
+  // const [info, setInfomation] = useState<Information | null>(null);
+  // 2. 상태의 타입이 까다로운 구조를 가진 객체 혹은 배열인 경우
+  // type Todo = {id: number; text: string; done: boolean};
+  // const [todos, setTodos] = useState<Todo[]>([]);
   const onIncrease = () => setCount(count + 1);
   const onDecrease = () => setCount(count - 1);
   return (
