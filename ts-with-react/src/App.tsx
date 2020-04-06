52734f6fac506b24e213ffa97b6388dc061096cb
jake <guswns1853@gmail.com>
2020-04-06 22:39:45 +0900

    useRef

--- ts-with-react/src/App.tsx
+++ ts-with-react/src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React from "react";
 import ReducerSample from "./ReducerSample";
+import MyForm from "./useRef";
 
 const App: React.FC = () => {
-  return <ReducerSample />
+  // return <ReducerSample />
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
+  };
+  return <MyForm onSubmit={onSubmit} />;
 };
 
 export default App;
